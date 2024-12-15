22f9c37d7585638316f78177b656eb47aa4aa322
drich1109 <aldrichbatislaon@gmail.com>
2024-12-16 00:11:23 +0800

    mod

--- src/routes/History.tsx
+++ src/routes/History.tsx
@@ -7,55 +7,61 @@ import HomeIcon from '../assets/svg/HomeIcon';
 
 type Props = StackScreenProps<RootStackParamList, 'History'>;
 
+const POLL_INTERVAL = 1000; 
+
 const History: React.FC<Props> = ({ navigation }) => {
   const [logs, setLogs] = useState<any[]>([]);
 
+  const fetchHistoryLogs = async () => {
+    try {
+      const snapshot = await firestore()
+        .collection('WaterLogs')
+        .orderBy('date', 'desc') 
+        .get();
+
+      const fetchedLogs = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        date: new Date(doc.data().date.seconds * 1000).toLocaleString(), 
+        humLevel: doc.data().humLevel,
+        tempLevel: doc.data().tempLevel,
+        moistLevel: doc.data().moistLevel,
+        method: doc.data().method,
+      }));
+      setLogs(fetchedLogs);
+    } catch (error) {
+      console.error('Error fetching history logs:', error);
+    }
+  };
+
   useEffect(() => {
-    const fetchHistoryLogs = async () => {
-      try {
-        const snapshot = await firestore()
-          .collection('WaterLogs')
-          .orderBy('date', 'desc') // Order logs by date in descending order
-          .get();
-
-        const fetchedLogs = snapshot.docs.map((doc) => ({
-          id: doc.id,
-          date: new Date(doc.data().date.seconds * 1000).toLocaleString(), // Convert Firestore timestamp
-          humLevel: doc.data().humLevel,
-          tempLevel: doc.data().tempLevel,
-          moistLevel: doc.data().moistLevel,
-          method: doc.data().method
-        }));
-        setLogs(fetchedLogs);
-      } catch (error) {
-        console.error('Error fetching history logs:', error);
-      }
-    };
-
     fetchHistoryLogs();
+
+    const intervalId = setInterval(() => {
+      fetchHistoryLogs(); 
+    }, POLL_INTERVAL);
+
+    return () => clearInterval(intervalId); 
   }, []);
 
   return (
     <SafeAreaView className="flex-1 bg-[#031527]">
-      {/* Top Navigation */}
+
       <View className="absolute top-10 w-full px-5 flex-row justify-between">
         <TouchableOpacity onPress={() => navigation.goBack()}>
           <HomeIcon className="h-6 w-6 text-white" />
         </TouchableOpacity>
       </View>
 
-      {/* Page Title */}
       <View className="px-6">
         <View className="items-center mt-16">
           <Text className="text-white text-2xl font-bold">History Logs</Text>
         </View>
-
       </View>
-      {/* History Logs Section */}
+
       <ScrollView className="mt-6 px-6" style={{ maxHeight: 700 }} showsVerticalScrollIndicator={false}>
         {logs.map((log) => (
           <View key={log.id} className="mb-6">
-            {/* Single History Item */}
+
             <View className="justify-between flex-row">
               <View className="flex-row items-center space-x-2">
                 <Text className="text-white text-base font-bold">Method:</Text>
@@ -83,7 +89,7 @@ const History: React.FC<Props> = ({ navigation }) => {
                 </Text>
               </View>
             </View>
-            {/* Border for multiple logs */}
+
             <View className="border-b-[1px] mx-1 border-white mt-8"></View>
           </View>
         ))}
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
+import { Alert, SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParamList } from '../../types';
 import firestore from '@react-native-firebase/firestore';
@@ -7,13 +7,14 @@ import { AnimatedCircularProgress } from "react-native-circular-progress";
 import { styled } from "nativewind";
 import DropsIcon from '../assets/svg/DropsIcon';
 import HistoryIcon from '../assets/svg/HistoryIcon';
+import DangerIcon from '../assets/svg/DangerIcon';
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
 
 type Props = StackScreenProps<RootStackParamList, 'Home'>;
 
-const POLL_INTERVAL = 5000; // Polling interval in milliseconds (e.g., 5000ms = 5 seconds)
+const POLL_INTERVAL = 1000; // Polling interval in milliseconds (e.g., 5000ms = 5 seconds)
 
 const HomeScreen: React.FC<Props> = ({ navigation }) => {
   const [moistLevel, setMoistLevel] = useState<number | null>(null);
@@ -24,57 +25,111 @@ const HomeScreen: React.FC<Props> = ({ navigation }) => {
 
   const fetchData = async () => {
     try {
-      const snapshot = await firestore()
+      const currentLevelSnapshot = await firestore()
         .collection('CurrentLevel')
         .orderBy('date', 'desc')
         .limit(1)
         .get();
 
-      if (!snapshot.empty) {
-        const data = snapshot.docs[0].data();
+      if (!currentLevelSnapshot.empty) {
+        const data = currentLevelSnapshot.docs[0].data();
         setMoistLevel(data.moistLevel);
         setHumLevel(data.humLevel);
         setTempLevel(data.tempLevel);
         setDate(new Date(data.date.seconds * 1000).toLocaleString());
+      }
+
+      // Fetch the latest WaterLogs data for isActive
+      const waterLogsSnapshot = await firestore()
+        .collection('WaterLogs')
+        .orderBy('date', 'desc')
+        .limit(1)
+        .get();
+
+      if (!waterLogsSnapshot.empty) {
+        const waterLogData = waterLogsSnapshot.docs[0].data();
+        setActiveWater(waterLogData.isActive);
       }
     } catch (error) {
       console.error('Error fetching data: ', error);
     }
   };
 
+
   useEffect(() => {
-    fetchData(); // Fetch data initially
+    fetchData();
 
     const intervalId = setInterval(() => {
-      fetchData(); // Fetch data at regular intervals
+      fetchData();
     }, POLL_INTERVAL);
 
-    return () => clearInterval(intervalId); // Cleanup interval on component unmount
+    return () => clearInterval(intervalId);
   }, []);
 
   const navigateToScreen = (screen: keyof RootStackParamList) => {
     navigation.navigate(screen);
   };
 
-  async function pressWater()
-  {
+  async function pressWater() {
     try {
-      setActiveWater(!activeWater)
-      console.log(activeWater)
-      const now = firestore.Timestamp.now();
-      await firestore().collection('WaterLogs').add({
-        isActive: activeWater,
-        date: now,
-        moistLevel: moistLevel || 0,
-        humLevel: humLevel || 0,
-        tempLevel: tempLevel || 0,
-        method: 'Manual',
-      });
-      console.log('Water action logged successfully with levels');
+      if (activeWater == false && tempLevel && humLevel && moistLevel && ((tempLevel <= 31 && humLevel >= 60) || moistLevel >= 40)) {
+        Alert.alert(
+          'Are you sure?',
+          'Are you sure you want to start/stop irrigating?',
+          [
+            {
+              text: 'No',
+              onPress: () => {
+                console.log('Irrigation canceled');
+                return;
+              },
+              style: 'cancel',
+            },
+            {
+              text: 'Yes',
+              onPress: async () => {
+                // Move the irrigation logic here
+                const newActiveState = !activeWater; // Toggle the state
+                setActiveWater(newActiveState);
+
+                const now = firestore.Timestamp.now();
+                await firestore().collection('WaterLogs').add({
+                  isActive: newActiveState,
+                  date: now,
+                  moistLevel: moistLevel || 0,
+                  humLevel: humLevel || 0,
+                  tempLevel: tempLevel || 0,
+                  method: 'Manual',
+                });
+
+                console.log('Water action logged successfully with levels');
+              },
+            },
+          ],
+          { cancelable: false }
+        );
+      } else {
+        const newActiveState = !activeWater; // Toggle the state
+        setActiveWater(newActiveState);
+
+        const now = firestore.Timestamp.now();
+        await firestore().collection('WaterLogs').add({
+          isActive: newActiveState,
+          date: now,
+          moistLevel: moistLevel || 0,
+          humLevel: humLevel || 0,
+          tempLevel: tempLevel || 0,
+          method: 'Manual',
+        });
+
+        console.log('Water action logged successfully with levels');
+      }
     } catch (error) {
       console.error('Error logging water action: ', error);
     }
   }
+
+
   return (
     <SafeAreaView className="flex-1">
       <View className="flex-1 justify-center p-4 bg-[#031527]">
@@ -87,7 +142,6 @@ const HomeScreen: React.FC<Props> = ({ navigation }) => {
             </TouchableOpacity>
           </View>
         </View>
-
         <View className="items-center">
           <StyledView className="flex-row space-x-8">
             <StyledView className="items-center">
@@ -100,9 +154,13 @@ const HomeScreen: React.FC<Props> = ({ navigation }) => {
                 lineCap="round"
                 rotation={0}>
                 {(fill) => (
-                  <StyledText className="text-white text-sm font-bold">
-                    {Math.round(fill)}%
-                  </StyledText>
+                  <StyledView className="items-center">
+                    {fill <= 40 && <DangerIcon className="text-red-400 h-4 w-4" />}
+                    <StyledText className="text-white text-sm font-bold">
+
+                      {Math.round(fill)}%
+                    </StyledText>
+                  </StyledView>
                 )}
               </AnimatedCircularProgress>
               <StyledText className="text-white text-lg mt-2">Moisture</StyledText>
@@ -118,9 +176,12 @@ const HomeScreen: React.FC<Props> = ({ navigation }) => {
                 lineCap="round"
                 rotation={0}>
                 {(fill) => (
-                  <StyledText className="text-white text-sm font-bold">
-                    {Math.round(fill)}%
-                  </StyledText>
+                  <StyledView className="items-center">
+                    {fill <= 60 && <DangerIcon className="text-red-400 h-4 w-4" />}
+                    <StyledText className="text-white text-sm font-bold">
+                      {Math.round(fill)}%
+                    </StyledText>
+                  </StyledView>
                 )}
               </AnimatedCircularProgress>
               <StyledText className="text-white text-lg mt-2">Humidity</StyledText>
@@ -137,24 +198,30 @@ const HomeScreen: React.FC<Props> = ({ navigation }) => {
               lineCap="round"
               rotation={0}>
               {(fill) => (
-                <StyledText className="text-white text-xl font-bold">
-                  {Math.round(fill)}°
-                </StyledText>
+                <StyledView className="items-center">
+                  {fill >= 32 && <DangerIcon className="text-red-400 h-6 w-6" />}
+                  <StyledText className="text-white text-xl font-bold">
+                    {Math.round(fill)}°
+                  </StyledText>
+                </StyledView>
               )}
             </AnimatedCircularProgress>
             <StyledText className="text-white text-lg mt-2">Temperature</StyledText>
           </StyledView>
 
-          <TouchableOpacity 
-            className="items-center mt-6 py-5 px-7 rounded-full" 
+          <TouchableOpacity
+            className="items-center mt-6 py-5 px-7 rounded-full"
             style={{ backgroundColor: 'rgba(22, 185, 255, 0.3)' }}
-            onPress={ () => {
-              pressWater()
+            onPress={() => {
+              pressWater();
             }}
           >
-            <DropsIcon className="text-white w-8 h-8" />
+            <DropsIcon
+              className={`w-8 h-8 ${activeWater ? 'text-red-400' : 'text-white'}`}
+            />
             <Text className="text-base text-white font-bold">Water</Text>
           </TouchableOpacity>
+
         </View>
       </View>
     </SafeAreaView>
