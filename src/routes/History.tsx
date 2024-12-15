5b79a60a8ec607d90256aea48e567be6b9f7d044
drich1109 <aldrichbatislaon@gmail.com>
2024-12-15 11:37:49 +0800

    display data

--- src/routes/History.tsx
+++ src/routes/History.tsx
@@ -1,207 +1,38 @@
-
 import React, { useState, useEffect } from 'react';
-import { RootStackParamList } from '../../types';
-import { StackScreenProps } from '@react-navigation/stack';
 import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import { StackScreenProps } from '@react-navigation/stack';
+import { RootStackParamList } from '../../types';
 import HomeIcon from '../assets/svg/HomeIcon';
-import CircleIcon from '../assets/svg/CircleIcon';
 
 type Props = StackScreenProps<RootStackParamList, 'History'>;
 
 const History: React.FC<Props> = ({ navigation }) => {
-  /*  const [modalVisible, setModalVisible] = useState(false);
-   const [leaderBoardVisible, setLeaderBoardVisible] = useState(false);
-   const [dailyTaskVisible, setdailyTaskVisible] = useState(false);
-   const [notificationVisible, setNotificationVisible] = useState(false);
-   const [currentSection, setCurrentSection] = useState<SectionDetails | null>(null);
-   const [activeScreen] = useState<keyof RootStackParamList | null>('Dashboard');
-   const [languageDetails, setLanguageDetails] = useState<Languages>();
-   const [userDetails, setUserDetails] = useState<UserProfileDto>();
-   const [fileUrl, setFileUrl] = useState<string | null>(null);
-   const unit = useNavigation<UnitScreenNavigationProp>();
-   const [sections, setSections] = useState<SectionDetails[]>([]);
-   const [dailyTasks, setdailyTasks] = useState<DailyTaskDto[]>([]);
-   const [expandedTasks, setExpandedTasks] = useState<{ [key: number]: boolean }>({});
-   const [notifications, setNotifications] = useState<NotificationsDto[]>([]);
-   const [notificationCount, setNotifCount] = useState<number>(0);
-   const [userId, setUserID] = useState<string>("");
-   const [isBuying, setIsBuying] = useState<boolean>(false);
-   const [loadingMessage, setLoadingMessage] = useState<string>("");
-   const [secWord, setSecWord] = useState<string>("");
-   const [notifWord, setNotifWord] = useState<string>("");
-   const [taskWord, setTaskWord] = useState<string>("");
-   const [letWord, setLetWord] = useState<string>("");
-   const [showSection, setShowSection] = useState<boolean>(true);
-   const [showNotif, setShowNotif] = useState<boolean>(true);
-   const [showTask, setShowTask] = useState<boolean>(true);
-   const [showLet, setShowLet] = useState<boolean>(true);
-   const toggleDescription = (taskID: number) => {
-     setExpandedTasks((prev) => ({
-       ...prev,
-       [taskID]: !prev[taskID],
-     }));
-   };
- 
-   const handleBackPress = () => {
-     BackHandler.exitApp();
-     return true;
-   };
- 
-   useEffect(() => {
-     const backHandler = BackHandler.addEventListener('hardwareBackPress', handleBackPress);
- 
-     return () => backHandler.remove();
-   }, []);
- 
- 
- 
-   const fetchUserData = async () => {
-     try {
-       const userID = await AsyncStorage.getItem('userID');
-       const languageID = await AsyncStorage.getItem('languageId');
-       console.log(languageID)
-       switch (languageID) {
-         case "1": setSecWord("Seksyon"); break;
-         case "2": setSecWord("Seksyon"); break;
-         case "3": setSecWord("Seksyon"); break;
-         default:
-           setSecWord("Section");
-           break;
-       }
-       switch (languageID) {
-         case "1": setNotifWord("Mga Pahibalo"); break;
-         case "2": setNotifWord("Abiso"); break;
-         case "3": setNotifWord("Mga Pahibaro"); break;
-         default:
-           setNotifWord("Notification");
-           break;
-       }
-       switch (languageID) {
-         case "1": setTaskWord("Buluhaton Sa Adlaw"); break;
-         case "2": setTaskWord("Buluhaton Sa Adlaw"); break;
-         case "3": setTaskWord("Buruhaton Sa Adlaw"); break;
-         default:
-           setTaskWord("Daily Task");
-           break;
-       }
-       switch (languageID) {
-         case "1": setLetWord("Sugdan Nato"); break;
-         case "2": setLetWord("Magsugod Kita"); break;
-         case "3": setLetWord("Magtikang Kita"); break;
-         default:
-           setLetWord("Let's Begin");
-           break;
-       }
-       setUserID(userID ?? "");
-       const result = await getUserLanguage(Number(userID));
-       setLanguageDetails(result.data);
-       const userResult = await getUserDetails(Number(userID));
-       setUserDetails(userResult.data);
-       const sectionResult = await getSections(Number(userID), result.data.languageID);
- 
-       setSections(sectionResult.data)
-       if (userResult.data.imagePath) {
-         setFileUrl(userResult.data.imagePath);
-       }
- 
-       const dailyTaskResult = await getDailyTasks(Number(userID));
-       console.log(dailyTaskResult)
-       setdailyTasks(dailyTaskResult.data);
- 
-       const notifResult = await getNotifications(Number(userID));
-       setNotifCount(notifResult.totalCount || 0);
-       setNotifications(notifResult.data);
-     } catch (error) {
-       console.error('Error retrieving user data:', error);
-     }
-   };
- 
-   useEffect(() => {
-     fetchUserData();
- 
-     // Toggle between 'Dictionary' and 'dictWord' every 3 seconds
-     const interval = setInterval(() => {
-       setShowSection((prev) => !prev);
-       setShowNotif((prev) => !prev);
-       setShowTask((prev) => !prev);
-       setShowLet((prev) => !prev);
-     }, 3000);
- 
-     // Cleanup interval on unmount
-     return () => clearInterval(interval);
-   }, []);
- 
-   useEffect(() => {
-     fetchUserData();
-     const unsubscribe = navigation.addListener('focus', fetchUserData);
-     return unsubscribe;
-   }, [navigation]);
- 
-   const navigateToUnit = () => {
-     if (currentSection) {
-       const sectionId = currentSection.sectionId
-       const sectionName = currentSection.sectionNumber.toString();
-       unit.navigate('Unit', { sectionId, sectionName });
-     }
-   };
- 
-   const navigateToLeaderboard = () => {
-     navigation.navigate("Leaderboard");
-   };
- 
- 
-   const openModal = (section: SectionDetails) => {
-     setCurrentSection(section);
-     setModalVisible(true);
-   };
- 
-   const closeModal = () => {
-     setModalVisible(false);
-     setCurrentSection(null);
-   };
- 
-   const opendailyTask = () => {
-     setdailyTaskVisible(true);
-   };
- 
-   const closedailyTask = () => {
-     setdailyTaskVisible(false);
-   };
- 
-   const closeLeaderBoard = () => {
-     setLeaderBoardVisible(false);
-   };
- 
-   const openNotification = async () => {
-     setNotificationVisible(true);
-   };
- 
-   const closeNotification = async () => {
-     await updateNotifications(userId);
-     setNotificationVisible(false);
-     fetchUserData();
-   };
- 
-   async function claimRewardDashboard(taskId: number) {
-     setIsBuying(true);
-     setLoadingMessage("Please wait...");
-     await claimReward(userId, taskId);
-     fetchUserData();
-     setIsBuying(false);
-   }
- 
-   const getTextSizeClass = (title: string) => {
-     const length = title.length;
-     if (length > 50) {
-       return 'text-sm';
-     } else if (length > 30) {
-       return 'text-lg';
-     } else {
-       return 'text-3xl';
-     }
-   };
- 
-   const circleSize = 100; */
+  const [logs, setLogs] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchHistoryLogs = async () => {
+      try {
+        const snapshot = await firestore()
+          .collection('WaterLogs')
+          .orderBy('date', 'desc') // Order logs by date in descending order
+          .get();
+
+        const fetchedLogs = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          date: new Date(doc.data().date.seconds * 1000).toLocaleString(), // Convert Firestore timestamp
+          humLevel: doc.data().humLevel,
+          tempLevel: doc.data().tempLevel,
+          moistLevel: doc.data().moistLevel,
+        }));
+        setLogs(fetchedLogs);
+      } catch (error) {
+        console.error('Error fetching history logs:', error);
+      }
+    };
+
+    fetchHistoryLogs();
+  }, []);
 
   return (
     <SafeAreaView className="flex-1 bg-[#031527]">
@@ -213,35 +44,44 @@ const History: React.FC<Props> = ({ navigation }) => {
       </View>
 
       {/* Page Title */}
+      <View className="px-6">
       <View className="items-center mt-16">
         <Text className="text-white text-2xl font-bold">History Logs</Text>
       </View>
-
+      <View className="border-b-[1px] mx-1 border-white mt-8 "></View>
+      </View>
       {/* History Logs Section */}
-      <View className="mt-10 px-6">
-        {/* Single History Item */}
-        <View className="mb-6">
-          <View className="items-end">
-            <Text className="text-white text-base">12-10-24 / 15:16</Text>
+      <View className="mt-6 px-6">
+        {logs.map((log) => (
+          <View key={log.id} className="mb-6">
+            {/* Single History Item */}
+            <View className="items-end">
+              <Text className="text-white text-base">{log.date}</Text>
+            </View>
+            <View className="mt-4 flex-row items-center justify-center space-x-8">
+              <View className="items-center">
+                <Text className="text-white text-lg">Moisture</Text>
+                <Text className="text-white text-lg font-semibold">
+                  {log.moistLevel}%
+                </Text>
+              </View>
+              <View className="items-center">
+                <Text className="text-white text-lg">Humidity</Text>
+                <Text className="text-white text-lg font-semibold">
+                  {log.humLevel}%
+                </Text>
+              </View>
+              <View className="items-center">
+                <Text className="text-white text-lg">Temperature</Text>
+                <Text className="text-white text-lg font-semibold">
+                  {log.tempLevel}°C
+                </Text>
+              </View>
+            </View>
+            {/* Border for multiple logs */}
+            <View className="border-b-[1px] mx-1 border-white mt-8"></View>
           </View>
-          <View className="mt-4 flex-row items-center justify-center space-x-8">
-            <View className="items-center">
-              <Text className="text-white text-lg">Moisture</Text>
-              <Text className="text-white text-lg font-semibold">44%</Text>
-            </View>
-            <View className="items-center">
-              <Text className="text-white text-lg">Humidity</Text>
-              <Text className="text-white text-lg font-semibold">46%</Text>
-            </View>
-            <View className="items-center">
-              <Text className="text-white text-lg">Temperature</Text>
-              <Text className="text-white text-lg font-semibold">32°</Text>
-            </View>
-          </View>
-        </View>
-        {/* mo gawas ra anag border if more than one na ang logs */}
-        <View className="border-b-[1px] mx-1 border-white"></View>
-        {/* You can add more history items here */}
+        ))}
       </View>
     </SafeAreaView>
   );
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,16 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { SafeAreaView, Text, TouchableOpacity, View, Image, BackHandler, Modal } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
-import { HomeScreenNavigationProp, RootStackParamList } from '../../types';
+import { RootStackParamList } from '../../types';
 import firestore from '@react-native-firebase/firestore';
 import { AnimatedCircularProgress } from "react-native-circular-progress";
 import { styled } from "nativewind";
 import HistoryIcon from '../assets/svg/HistoryIcon';
-import NotificationIcon from '../assets/svg/NotificationIcon';
-import SettingIcon from '../assets/svg/SettingIcon';
-import SettingsIcon from '../assets/svg/SettingsIcon';
 import DropsIcon from '../assets/svg/DropsIcon';
-import CircleIcon from '../assets/svg/CircleIcon';
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
@@ -18,80 +14,49 @@ const StyledText = styled(Text);
 type Props = StackScreenProps<RootStackParamList, 'Home'>;
 
 const HomeScreen: React.FC<Props> = ({ navigation }) => {
+  const [moistLevel, setMoistLevel] = useState<number | null>(null);
+  const [humLevel, setHumLevel] = useState<number | null>(null);
+  const [tempLevel, setTempLevel] = useState<number | null>(null);
+  const [date, setDate] = useState<string | null>(null);
 
-  /*  const [test, setTest] = useState();
- 
-   const getData = async () => {
-     const testCollection = await firestore().collection('Test').get();
-     setTest(testCollection.docs[0].data())
-   }
- 
-   useEffect(() => {
-     getData();
-   }, []); */
-  const [notificationVisible, setNotificationVisible] = useState(false);
-  const [currentDate, setCurrentDate] = useState<string>('');
-  const [currentTime, setCurrentTime] = useState<string>('');
   const navigateToScreen = (screen: keyof RootStackParamList) => {
     navigation.navigate(screen);
   };
-  useEffect(() => {
-    const updateDateTime = () => {
-      const now = new Date();
-
-      const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-      const day = days[now.getDay()];
-
-      const date = now.toLocaleDateString('en-US', {
-        month: 'long',
-        day: 'numeric',
-      });
-
-      const time = now.toLocaleTimeString('en-US', {
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false,
-      });
-
-      setCurrentDate(`${day}, ${date}`);
-      setCurrentTime(time);
-    };
-
-    const intervalId = setInterval(updateDateTime, 1000);
-    updateDateTime();
-
-    return () => clearInterval(intervalId);
-  }, []);
 
   useEffect(() => {
-    const backAction = () => {
-      BackHandler.exitApp();
-      return true;
+    const fetchData = async () => {
+      try {
+        const snapshot = await firestore()
+          .collection('CurrentLevel')
+          .orderBy('date', 'desc') // Fetch the latest entry
+          .limit(1)
+          .get();
+
+        if (!snapshot.empty) {
+          const data = snapshot.docs[0].data();
+          setMoistLevel(data.moistLevel);
+          setHumLevel(data.humLevel);
+          setTempLevel(data.tempLevel);
+          setDate(new Date(data.date.seconds * 1000).toLocaleString()); // Convert Firestore Timestamp to readable date
+        }
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
     };
 
-    const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
-
-    return () => backHandler.remove();
-
+    fetchData();
   }, []);
-
-  const openNotification = async () => {
-    setNotificationVisible(true);
-  };
-
-  const closeNotification = async () => {
-    setNotificationVisible(false);
-  };
 
   return (
     <SafeAreaView className="flex-1">
       <View className="flex-1 justify-center p-4 bg-[#031527]">
         <View className="absolute top-6 w-full px-4">
           <View className="flex-row items-center justify-between">
-            <View>
-              <Text className="text-white font-bold text-lg">{currentDate}</Text>
-            </View>
-            <Text className="text-white text-xl font-bold">{currentTime}</Text>
+            <Text className="text-white text-xl font-bold">{date || 'Loading...'}</Text>
+            <TouchableOpacity className="items-center flex-row space-x-2" onPress={() => navigateToScreen('History')}>
+              <HistoryIcon className="text-white h-6 w-6" />
+              <Text className="text-base text-white font-bold">History</Text>
+            </TouchableOpacity>
           </View>
         </View>
         <View className="items-center">
@@ -100,7 +65,7 @@ const HomeScreen: React.FC<Props> = ({ navigation }) => {
               <AnimatedCircularProgress
                 size={100}
                 width={10}
-                fill={35}
+                fill={moistLevel || 0}
                 tintColor="#00FF93"
                 backgroundColor="#1E293B"
                 lineCap="round"
@@ -118,7 +83,7 @@ const HomeScreen: React.FC<Props> = ({ navigation }) => {
               <AnimatedCircularProgress
                 size={100}
                 width={10}
-                fill={45}
+                fill={humLevel || 0}
                 tintColor="#FF6666"
                 backgroundColor="#1E293B"
                 lineCap="round"
@@ -137,7 +102,7 @@ const HomeScreen: React.FC<Props> = ({ navigation }) => {
             <AnimatedCircularProgress
               size={120}
               width={10}
-              fill={69}
+              fill={tempLevel || 0}
               tintColor="#00FFAB"
               backgroundColor="#1E293B"
               lineCap="round"
@@ -150,57 +115,32 @@ const HomeScreen: React.FC<Props> = ({ navigation }) => {
             </AnimatedCircularProgress>
             <StyledText className="text-white text-lg mt-2">Temperature</StyledText>
           </StyledView>
-          <View className="flex-row items-center justify-center space-x-2 mt-8">
-            <TouchableOpacity className="items-center p-4 rounded-xl" onPress={() => navigateToScreen('History')} style={{ backgroundColor: 'rgba(255, 255, 255, 0.3)' }}>
-              <HistoryIcon className="text-white h-6 w-6" />
-              <Text className="text-base text-white font-bold">History</Text>
-            </TouchableOpacity>
-            <TouchableOpacity className="items-center p-4 rounded-xl" onPress={openNotification} style={{ backgroundColor: 'rgba(255, 255, 255, 0.3)' }}>
-              <NotificationIcon className="text-white h-6 w-6" />
-              <Text className="text-base text-white font-bold">Notifications</Text>
-            </TouchableOpacity>
-            <TouchableOpacity className="items-center p-4 rounded-xl" onPress={() => navigateToScreen('Setting')} style={{ backgroundColor: 'rgba(255, 255, 255, 0.3)' }}>
-              <SettingsIcon className="text-white h-6 w-6" />
-              <Text className="text-base text-white font-bold">Settings</Text>
-            </TouchableOpacity>
-          </View>
-          <TouchableOpacity className="items-center mt-6 py-5 px-7 rounded-full" style={{ backgroundColor: 'rgba(22, 185, 255, 0.3)' }}>
+
+          <TouchableOpacity 
+            className="items-center mt-6 py-5 px-7 rounded-full" 
+            style={{ backgroundColor: 'rgba(22, 185, 255, 0.3)' }}
+            onPress={async () => {
+              try {
+                const now = firestore.Timestamp.now();
+                await firestore().collection('WaterLogs').add({
+                  isActive: true,
+                  date: now,
+                  moistLevel: moistLevel || 0,
+                  humLevel: humLevel || 0,
+                  tempLevel: tempLevel || 0,
+                  method: 'manual',
+                });
+                console.log('Water action logged successfully with levels');
+              } catch (error) {
+                console.error('Error logging water action: ', error);
+              }
+            }}
+          >
             <DropsIcon className="text-white w-8 h-8" />
             <Text className="text-base text-white font-bold">Water</Text>
           </TouchableOpacity>
         </View>
-        {/* <Text className="text-white text-xl">{test}</Text> */}
       </View>
-
-      <Modal
-        visible={notificationVisible}
-        transparent={true}
-        animationType="slide"
-        onRequestClose={closeNotification}
-      >
-        <TouchableOpacity
-          className="flex-1 justify-center items-center"
-          style={{ backgroundColor: 'rgba(0, 0, 0, 0.45)' }}
-          onPress={closeNotification}
-        >
-          <View className="flex-1 justify-center items-center bg-opacity-50">
-            <TouchableOpacity activeOpacity={1}>
-              <View className="bg-[#031527] rounded-2xl py-3 px-6 shadow-lg">
-                <Text className="text-white text-2xl text-center">Notifications</Text>
-                <View className="mt-4 mb-4">
-                  <View className="flex-row items-center space-x-2">
-                    <CircleIcon className="text-blue-400 h-4 w-4" />
-                    <Text className="text-white text-lg">Watered</Text>
-                    <Text className="text-white text-base pl-8">1 Hour Ago</Text>
-                  </View>
-                </View>
-                {/* mo gawas ra anag border if more than one na ang notif */}
-                <View className="border-b-[1px] mx-1 border-white"></View>
-              </View>
-            </TouchableOpacity>
-          </View>
-        </TouchableOpacity>
-      </Modal>
     </SafeAreaView>
   );
 };
